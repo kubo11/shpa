dac5d2bfb0b937e39347dcdc6945b83973066a38
kubo11 <jakub.obroslak@onet.pl>
2024-05-13 16:25:32 +0200

    Update example_generator.py interface

--- example_generator.py
+++ example_generator.py
@@ -1,13 +1,11 @@
-from sys import argv
+from argparse import ArgumentParser, Namespace
+from pathlib import Path
 from random import randint
 
-MIN_WEIGHT = 1
-MAX_WEIGHT = 10_000
-
 class RandomIncidenceArray:
-    def __init__(self, n: int) -> None:
+    def __init__(self, n: int, min_weight: int, max_weight: int) -> None:
         self._incidence_array = \
-            [[randint(MIN_WEIGHT, MAX_WEIGHT) for col in range(row+1)] for row in range(n-1)]
+            [[randint(min_weight, max_weight) for col in range(row+1)] for row in range(n-1)]
 
     def __str__(self) -> str:
         out = str(len(self._incidence_array)+1)
@@ -23,18 +21,25 @@ def print_usage() -> None:
     print('\tsize - number of vertices for which graph should be generated')
     print('\tpath (optional) - path to output file (default: test_<size>.txt)')
 
+def parse_args() -> Namespace:
+    parser = ArgumentParser()
+    parser.add_argument('-v', '--vertices', type=int, required=True, help='number of vertices for which graph should be generated')
+    parser.add_argument('-o', '--output', type=Path, required=False, default=None, help='path to output file (default: test_<size>.txt)')
+    parser.add_argument('-m', '--min-weight', type=int, required=False, default=1, help='minimal weight')
+    parser.add_argument('-x', '--max-weight', type=int, required=False, default=100, help="maximal weight")
+
+    return parser.parse_args()
+
 def save(r: RandomIncidenceArray, path: str) -> None:
     with open(path, "w") as file:
         file.write(str(r))
 
 def main() -> None:
-    if len(argv) == 2:
-        save(RandomIncidenceArray(int(argv[1])), f'test{argv[1]}.txt')
-    elif len(argv) == 3:
-        save(RandomIncidenceArray(int(argv[1])), argv[2])
-    else:
-        print('Invalid number of arguments')
-        print_usage()
+    args = parse_args()
+    if args.output is None:
+        args.output = f'test{args.vertices}.txt'
+
+    save(RandomIncidenceArray(args.vertices, args.min_weight, args.max_weight), args.output)
 
 if (__name__ == '__main__'):
     main()
